1e83b855488b533ba8ab4352440d45897100bfdf
ikachu-7 <vivek.a.tiwari13@gmail.com>
2023-06-16 14:53:29 +0530

    Animation

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,20 +1,65 @@
-import React from "react";
+// import React from "react";
+// import { NavLink, useNavigate } from "react-router-dom";
+
+// const Navbar = () => {
+//   const navigate = useNavigate()
+
+//   return (
+//     <div>
+//       <nav className="nav-bar">
+//       <h1 className="navbar-logo"><span style={{fontSize:"40px"}}>🏦</span> Silvergate</h1>
+//         <div className="nav-items">
+//           <NavLink to="/">Home</NavLink>
+//           <NavLink to="/history">History</NavLink>
+//           <NavLink to="*">blog</NavLink>
+//           <NavLink to="*">contact</NavLink>
+//         </div>
+//         <button onClick={()=>navigate('/transaction',{state: {NAME:"",EMAIL:"",BANK:""}})}>Transaction</button>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleMenu = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <div>
-      <nav className="nav-bar">
-      <h1 className="navbar-logo"><span style={{fontSize:"40px"}}>🏦</span> Silvergate</h1>
-        <div className="nav-items">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/history">History</NavLink>
-          <NavLink to="*">blog</NavLink>
-          <NavLink to="*">contact</NavLink>
+      <nav className={`nav-bar ${expanded ? "expanded" : ""}`}>
+        <h1 className="navbar-logo">
+          <span style={{ fontSize: "40px" }}>🏦</span> Silvergate
+        </h1>
+        <div className={`nav-items ${expanded ? "expanded" : ""}`}>
+          <NavLink exact to="/" onClick={toggleMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/history" onClick={toggleMenu}>
+            History
+          </NavLink>
+          <NavLink to="*" onClick={toggleMenu}>
+            Blog
+          </NavLink>
+          <NavLink to="*" onClick={toggleMenu}>
+            Contact
+          </NavLink>
         </div>
-        <button onClick={()=>navigate('/transaction',{state: {NAME:"",EMAIL:"",BANK:""}})}>Transaction</button>
+        <button onClick={() => navigate("/transaction", { state: { NAME: "", EMAIL: "", BANK: "" } })}>
+          Transaction
+        </button>
+        <div className={`menu-toggle ${expanded ? "expanded" : ""}`} onClick={toggleMenu}>
+          <div className="menu-bar"></div>
+          <div className="menu-bar"></div>
+          <div className="menu-bar"></div>
+        </div>
       </nav>
     </div>
   );
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,16 +1,72 @@
-import React from "react";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// const Home = () => {
+//   const navigate = useNavigate();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     navigate("/allusers");
+//   };
+//   return (
+//     <>
+//       <div>
+//         <header className="hero-section">
+//           <div className="hero-text-container">
+//             <h1>
+//               Next generation
+//               <br />
+//               digital banking
+//             </h1>
+//             <p>
+//               Take your financial life online. Your easy bank account
+//               <br />
+//               will be a one-stop-shop for spending,saving,
+//               <br />
+//               budgeting,investing, and much more.
+//             </p>
+//             <button onClick={handleSubmit}>View All customers</button>
+//           </div>
+//           <div className="hero-img-container">
+//             <img src="Home.jpg" alt="hero"/>
+//           </div>
+//         </header>
+//         <div className="why-container">
+//           <section className="why-us">
+//             <h1>Why choose Silvergate?</h1>
+//             <p>
+//               We leverage open banking to turn your bank account into your
+//               financial hub.
+//               <br />
+//               Control your finances like never before.
+//             </p>
+//           </section>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Home;
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 const Home = () => {
   const navigate = useNavigate();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate("/allusers");
   };
+
   return (
     <>
       <div>
-        <header className="hero-section">
-          <div className="hero-text-container">
+        <header className={`hero-section ${loaded ? "loaded" : ""}`}>
+          <div className={`hero-text-container ${loaded ? "loaded" : ""}`}>
             <h1>
               Next generation
               <br />
@@ -19,14 +75,14 @@ const Home = () => {
             <p>
               Take your financial life online. Your easy bank account
               <br />
-              will be a one-stop-shop for spending,saving,
+              will be a one-stop-shop for spending, saving,
               <br />
-              budgeting,investing, and much more.
+              budgeting, investing, and much more.
             </p>
             <button onClick={handleSubmit}>View All customers</button>
           </div>
-          <div className="hero-img-container">
-            <img src="Home.jpg" alt="hero"/>
+          <div className={`hero-img-container ${loaded ? "loaded" : ""}`}>
+            <img src="Home.jpg" alt="hero" />
           </div>
         </header>
         <div className="why-container">
